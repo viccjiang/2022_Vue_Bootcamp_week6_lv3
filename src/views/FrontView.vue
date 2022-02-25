be8d286673901c5118b4b39a22a79f4562cec355
jiangvicc <binbinbb520@gmail.com>
2022-02-25 15:03:51 +0800

    “add products

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <router-link to="/" class="navbar-brand">Piincool 品酷家居</router-link>
+      <router-link to="/" class="navbar-brand">LOGO here</router-link>
       <button
         class="navbar-toggler"
         type="button"
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,3 +1,45 @@
 <template>
   <h2>產品列表</h2>
+  <div class="container">
+    <div class="row row-cols-1 row-cols-lg-4 g-3">
+      <div class="col" v-for="product in products" :key="product.id">
+        <div class="card h-100">
+          <img :src="product.imageUrl" class="card-img-top" alt="..." />
+          <div class="card-body">
+            <h5 class="card-title">{{ product.title }}</h5>
+            <p class="card-text">
+              {{ product.description }}
+            </p>
+            <router-link :to="`/product/${product.id}`" class="btn btn-primary">
+              Go somewhere
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    grtProducts() {
+      this.$http(`${process.env.VUE_APP_API}api/${process.env.VUE_APP_PATH}/products/all`)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.dir(err.response.data.message);
+        });
+    },
+  },
+  mounted() {
+    this.grtProducts();
+  },
+};
+</script>
